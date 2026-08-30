f8c7120d77769e7339b4d57cf795b2366c0297b9
ossputraualia-sudo <oss.putraualia@gmail.com>
2026-08-30 20:04:44 +0700

    Update OneSignalSDKWorker.js with badge notification handlers

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,15 @@
 importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+// Pasang angka badge di taskbar saat push notifikasi masuk
+self.addEventListener('push', function(event) {
+  if ('setAppBadge' in self.navigator) {
+    self.navigator.setAppBadge(1).catch(function(e){});
+  }
+});
+
+// Bersihkan badge saat notifikasi diklik
+self.addEventListener('notificationclick', function(event) {
+  if ('clearAppBadge' in self.navigator) {
+    self.navigator.clearAppBadge().catch(function(e){});
+  }
+});
