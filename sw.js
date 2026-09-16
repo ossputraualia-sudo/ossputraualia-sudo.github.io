5d6b0d9129207c39fddce7f8ac369e19f47897a1
ossputraualia-sudo <oss.putraualia@gmail.com>
2026-09-16 12:19:18 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+// Service Worker Native PAG Docs (WhatsApp-style Notification)
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(clients.claim());
+});
+
+// Ketika banner notifikasi Windows diklik
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  var targetUrl = (event.notification.data && event.notification.data.url) ? event.notification.data.url : '/';
+
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+      for (var i = 0; i < clientList.length; i++) {
+        var client = clientList[i];
+        if ('focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(targetUrl);
+      }
+    })
+  );
+});
